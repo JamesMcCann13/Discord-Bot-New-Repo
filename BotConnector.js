4cc7babd3a14b3d50003f2d729908da56627064b
James McCann <jamestmccann2006@icloud.com>
2025-08-15 17:55:23 +0100

    Installed express

--- BotConnector.js
+++ BotConnector.js
@@ -1,6 +1,16 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(PORT, () => console.log(`HTTP server listening on port ${PORT}`));
+
+
+
 const BOT_TOKEN = process.env.BOT_TOKEN; // Replace with your bot token
 const GENERAL_CHANNEL_ID = '1358550826788065436';
 const USERID = '696071031953162270'; //XThejamabronX
